af79b4fdb7de74debca59a2a717aa1bd9c319347
Forest Good <fgoodiu@gmail.com>
2013-10-22 15:52:17 -0400

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save	
+      redirect_to root_url
+    end
+  end
+
+  def index
+  	@users = User.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Pingpong::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root to: 'static_pages#home'
   # The priority is based upon order of creation:
