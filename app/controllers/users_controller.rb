8b0c3f1e93ec2cba8675dd62ffa320f9af36b0ec
Forest Good <fgoodiu@gmail.com>
2013-10-23 15:30:53 -0400

    Update copy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save	
-      flash[:success] = "Thank you for registering.  Get excited for WeWork PingPong!"
+      flash[:success] = "Thank you for registering for the WeWork pingpong tournament. If you have any questions please contact us pingpong@6ps.com."
       redirect_to root_url
     else
-      flash[:error] = "Please make sure all fields are filled out"
+      flash[:error] = "Please make sure all required fields are filled out and your email address is unique."
       redirect_to root_url
     end
   end
